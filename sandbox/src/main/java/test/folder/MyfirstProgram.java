35bc49c1f083b37b63f8406fb16c2d7084c98794
ihamzova <ihamzova@yandex.ru>
2020-11-29 16:09:28 +0300

    Убрать слэш в гитигнор

--- sandbox/src/main/java/test/folder/MyfirstProgram.java
+++ sandbox/src/main/java/test/folder/MyfirstProgram.java
@@ -1,9 +1,12 @@
 package test.folder;
 
-public class MyfirstProgram
-{
+public class MyfirstProgram {
 
-    public static void main(String[] args) {
-        System.out.println("Hello, world!");
-    }
+  public static void main(String[] args) {
+    String somebody = "world";
+    System.out.println("Hello," + somebody+ "!");
+    double l=8.0;
+    double s=l*l;
+    System.out.println("Площадь квадрата со стороной" + l + "=" +s);
+  }
 }

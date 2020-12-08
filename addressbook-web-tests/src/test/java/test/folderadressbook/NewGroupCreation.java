ae344ec04bb7c14b430a277f237f6f4899cb0935
ihamzova <ihamzova@yandex.ru>
2020-12-08 17:16:36 +0300

    DZ4. Create new User and Group
    v2. Method added.

--- addressbook-web-tests/src/test/java/test/folderadressbook/NewGroupCreation.java
+++ addressbook-web-tests/src/test/java/test/folderadressbook/NewGroupCreation.java
@@ -5,7 +5,7 @@ import org.openqa.selenium.NoAlertPresentException;
 import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
@@ -70,7 +70,7 @@ public class NewGroupCreation {
     wd.findElement(By.linkText("groups")).click();
   }
 
-  @AfterClass(alwaysRun = true)
+  @AfterMethod(alwaysRun = true)
   public void tearDown() throws Exception {
     wd.quit();
   }

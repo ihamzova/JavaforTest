8cc752b8a30f64130d810dc116e1efcb1a5f1f27
Khamzova Irina <Irina.Khamzova@t-systems.ru>
2020-12-10 14:15:34 +0300

    NavigationHelper

--- addressbook-web-tests/src/test/java/test/folderadressbook/tests/DeleteGroup.java
+++ addressbook-web-tests/src/test/java/test/folderadressbook/tests/DeleteGroup.java
@@ -6,10 +6,10 @@ public class DeleteGroup extends Testbase {
 
   @Test
   public void testDeleteGroup() throws Exception {
-    app.goToGroupPage();
-    app.selectGroup();
-    app.deleteSelectedGroups();
-    app.returnToGroupPage();
+    app.getNavigationHelper().goToGroupPage();
+    app.getGroupHelper().selectGroup();
+    app.getGroupHelper().deleteSelectedGroups();
+    app.getGroupHelper().returnToGroupPage();
   }
 
 }

eb8e814d522cef710ad68b915dd3ba4a43670647
Khamzova Irina <Irina.Khamzova@t-systems.ru>
2020-12-10 14:38:18 +0300

    Revert "NavigationHelper"
    This reverts commit 8cc752b8a30f64130d810dc116e1efcb1a5f1f27.

--- addressbook-web-tests/src/test/java/test/folderadressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/test/folderadressbook/appmanager/ApplicationManager.java
@@ -5,28 +5,58 @@ import org.openqa.selenium.NoAlertPresentException;
 import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import test.folderadressbook.model.GroupData;
 
 import java.util.concurrent.TimeUnit;
 
-public class ApplicationManager {
-  WebDriver wd;
-
-  private NavigationHelper navigationHelper;
-  private GroupHelper groupHelper;
-  private SessionHelper sessionHelper;
+public class ApplicationManager extends GroupHelper {
+  public WebDriver wd;
 
   public void init() {
     System.setProperty("webdriver.chrome.driver", "C:\\chromedriver.exe");
     wd = new ChromeDriver();
     wd.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
     wd.get("http://localhost:8080/addressbook/group.php");
-    groupHelper = new GroupHelper(wd);
-    navigationHelper = new NavigationHelper(wd);
-    sessionHelper=new SessionHelper(wd);
-    sessionHelper.login("admin", "secret");
+    login("admin", "secret");
   }
 
+  public void login(String username, String password) {
+    wd.findElement(By.name("user")).click();
+    wd.findElement(By.name("user")).clear();
+    wd.findElement(By.name("user")).sendKeys(username);
+    wd.findElement(By.name("pass")).click();
+    wd.findElement(By.name("pass")).clear();
+    wd.findElement(By.name("pass")).sendKeys(password);
+    wd.findElement(By.xpath("//input[@value='Login']")).click();
+  }
 
+  public void returnToGroupPage() {
+    wd.findElement(By.linkText("group page")).click();
+  }
+
+  public void submitGroupCreation() {
+    wd.findElement(By.name("submit")).click();
+  }
+
+  public void fillGroupForm(GroupData groupData) {
+    wd.findElement(By.name("group_name")).click();
+    wd.findElement(By.name("group_name")).clear();
+    wd.findElement(By.name("group_name")).sendKeys(groupData.getName());
+    wd.findElement(By.name("group_header")).click();
+    wd.findElement(By.name("group_header")).clear();
+    wd.findElement(By.name("group_header")).sendKeys(groupData.getHeader());
+    wd.findElement(By.name("group_footer")).click();
+    wd.findElement(By.name("group_footer")).clear();
+    wd.findElement(By.name("group_footer")).sendKeys(groupData.getFooter());
+  }
+
+  public void initGroupCreation() {
+    wd.findElement(By.name("new")).click();
+  }
+
+  public void goToGroupPage() {
+    wd.findElement(By.linkText("groups")).click();
+  }
 
   public void stop() {
     wd.quit();
@@ -50,11 +80,11 @@ public class ApplicationManager {
     }
   }
 
-  public GroupHelper getGroupHelper() {
-    return groupHelper;
+  public void deleteSelectedGroups() {
+    wd.findElement(By.name("delete")).click();
   }
 
-  public NavigationHelper getNavigationHelper() {
-    return navigationHelper;
+  public void selectGroup() {
+    wd.findElement(By.xpath("(//input[@name='selected[]'])[2]")).click();
   }
 }
--- addressbook-web-tests/src/test/java/test/folderadressbook/tests/NewGroupCreation.java
+++ addressbook-web-tests/src/test/java/test/folderadressbook/tests/NewGroupCreation.java
@@ -7,11 +7,11 @@ public class NewGroupCreation extends Testbase {
 
   @Test
   public void testCreateNewGroup() throws Exception {
-    app.getNavigationHelper().goToGroupPage();
-    app.getGroupHelper().initGroupCreation();
-    app.getGroupHelper().fillGroupForm(new GroupData("Testgroup3", "Test Group for lesson 2", "TestGroup"));
-    app.getGroupHelper().submitGroupCreation();
-    app.getGroupHelper().returnToGroupPage();
+    app.goToGroupPage();
+    app.initGroupCreation();
+    app.fillGroupForm(new GroupData("Testgroup3", "Test Group for lesson 2", "TestGroup"));
+    app.submitGroupCreation();
+    app.returnToGroupPage();
   }
 
 }

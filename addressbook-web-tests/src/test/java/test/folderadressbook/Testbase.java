d080290e4f592db930105977c9ff78af0f1a13ba
Khamzova Irina <Irina.Khamzova@t-systems.ru>
2020-12-10 17:39:31 +0300

    Navigation9

--- addressbook-web-tests/src/test/java/test/folderadressbook/Testbase.java
+++ addressbook-web-tests/src/test/java/test/folderadressbook/Testbase.java
@@ -7,7 +7,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeMethod;
-
+import test.folderadressbook.model.GroupData;
 import java.util.concurrent.TimeUnit;
 
 public class Testbase {

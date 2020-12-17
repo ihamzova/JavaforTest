a407cd0b35145509ed54d89f1ab97119902d7b34
Khamzova Irina <Irina.Khamzova@t-systems.ru>
2020-12-17 18:33:08 +0300

    Create/Delete new User

--- addressbook-web-tests/src/test/java/test/folderadressbook/tests/tests/CreateNewUser.java
+++ addressbook-web-tests/src/test/java/test/folderadressbook/tests/tests/CreateNewUser.java
@@ -1,12 +1,11 @@
 package test.folderadressbook.tests.tests;
 
 import org.testng.annotations.Test;
-import test.folderadressbook.tests.appmanager.ApplicationManager;
 import test.folderadressbook.tests.model.UserData;
 
 public class CreateNewUser extends Testbase {
 
-  private final ApplicationManager app = new ApplicationManager();
+  //private final ApplicationManager app = new ApplicationManager();
 
   @Test
   public void testCreateNewUser() throws Exception {
--- addressbook-web-tests/src/test/java/test/folderadressbook/tests/tests/DeleteUser.java
+++ addressbook-web-tests/src/test/java/test/folderadressbook/tests/tests/DeleteUser.java
@@ -5,11 +5,11 @@ import test.folderadressbook.tests.appmanager.ApplicationManager;
 
 public class DeleteUser extends Testbase {
 
-  private final ApplicationManager app = new ApplicationManager();
+  //private final ApplicationManager app = new ApplicationManager();
 
   @Test
   public void testDeleteUser() throws Exception {
-   // goToHomePage();
+    app.navigationHelper.returnHomePage();
     app.userHelper.selectUser("27");
     app.userHelper.deleteSelectedUser();
     app.navigationHelper.closeAlertPage();

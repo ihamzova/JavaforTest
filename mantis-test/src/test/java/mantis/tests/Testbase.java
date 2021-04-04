c67062994b64bb4903a7e1ac9c9cb35cbd00c7ff
ikhamzov <irina.khamzova@t-systems.com>
2021-04-04 19:52:54 +0300

    Задание №17: Подготовить новый проект для нового учебного приложения

---
+++ mantis-test/src/test/java/mantis/tests/Testbase.java
@@ -0,0 +1,21 @@
+package mantis.tests;
+
+import mantis.appmanager.ApplicationManager;
+import org.openqa.selenium.remote.BrowserType;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
+
+public class Testbase {
+
+  protected static final ApplicationManager app = new ApplicationManager(System.getProperty("browser", BrowserType.CHROME));
+
+  @BeforeSuite(alwaysRun = true)
+  public void setUp() throws Exception {
+    app.init();
+  }
+
+  @AfterSuite(alwaysRun = true)
+  public void tearDown() throws Exception {
+    app.stop();
+  }
+}
